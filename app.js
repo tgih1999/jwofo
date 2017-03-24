962a94a481967071ee17423e90963227bf442e78
EricLin <tgih1999@gmail.com>
2017-03-23 23:21:57 -0700

    finished

---
+++ app.js
@@ -0,0 +1,42 @@
+(function() {
+  const config = {
+    apiKey: "AIzaSyCgh3nk8nxUoqFir2LjKYuYiYaRPtQKT0c",
+    authDomain: "ofome-f7639.firebaseapp.com",
+    databaseURL: "https://ofome-f7639.firebaseio.com",
+    storageBucket: "ofome-f7639.appspot.com",
+    messagingSenderId: "181493512430"
+  };
+  
+  firebase.initializeApp(config);
+
+  submit_button = document.getElementById('submit_button');
+
+  if ( submit_button){
+
+    submit_button.addEventListener('click', e => {
+      const name = document.getElementById('name_text');
+      const number = document.getElementById('number_text');
+      const password = document.getElementById('pass_text');
+      const new_text = document.getElementById('submit_text');
+
+      var pass, num;
+      var temp = firebase.database().ref('bikeObject/password');
+
+      temp.on('value', function(snapshot) {
+        pass = snapshot.val();
+
+        var temp = firebase.database().ref('bikeObject/' + number.value);
+        temp.on('value', function(snapshot) {
+          num = snapshot.val();
+
+          if (password.value == pass && num != undefined){
+            new_text.innerHTML = num;
+          }
+          else{
+            new_text.innerHTML = "Invalid Entry";
+          }
+        });
+      });
+    });
+  }
+}());
